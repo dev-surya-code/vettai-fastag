585d79900b353c30701b8e0f268d8c2a04742dbd
Surya <suryadeveloper02@gmail.com>
2025-10-15 01:01:54 +0530

    Update WorkerSignup.jsx

--- client/src/pages/WorkerSignup.jsx
+++ client/src/pages/WorkerSignup.jsx
@@ -7,7 +7,7 @@ import {
   FaUser,
   FaLock,
 } from "react-icons/fa";
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = process.env.REACT_APP_API_URL || "https://vettai-fastag-backend.onrender.com";
 import { useNavigate } from "react-router-dom";
 import "../css/WorkerSignup.css";
 

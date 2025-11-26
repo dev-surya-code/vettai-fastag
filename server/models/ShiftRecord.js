6a08e7d3537731f443963e8c6d1f104ad41bfd17
Surya <suryadeveloper02@gmail.com>
2025-11-26 15:50:03 +0530

    Make 'shiftCloseTime' a required field
    Added required field 'shiftCloseTime' to shift schema.

--- server/models/ShiftRecord.js
+++ server/models/ShiftRecord.js
@@ -4,7 +4,7 @@ const shiftSchema = new Schema({
   worker: { type: String, required: true },
   shiftType: { type: String, enum: ["DAY", "NIGHT"], required: true },
   loginTime: { type: Date, default: Date.now },
-  shiftCloseTime: { type: Date },
+  shiftCloseTime: { type: Date, required: true },
   bankBalances: { type: Object, default: {} },
   totalsByPaymentType: { type: Object, default: {} },
   transactions: { type: Array, default: [] },
